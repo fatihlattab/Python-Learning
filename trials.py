78047c4c88be6256869bb108b89f998aa3a9b0e2
Fatih Battal <fatihbattal@Fatih-MacBook-Pro.local>
2021-02-14 06:15:11 +0300

    Python iş ilanına başvuracak seviyede bilgiler

--- trials.py
+++ trials.py
@@ -2,12 +2,12 @@ print("Merhabalar")
 print("Ben, Fatih!")
 print("Bu benim ilk GitHub + Python Kod serüvenim")
 print("Hakkımda Hayırlısı!")
-cevap=input("Başarılı Olur Muyum?")
+cevap=input("Başarılı Olur Muyum? \n")
 if cevap=="hayir":
     print("Olamaz mışım...")
 if cevap=="evet":
     print("En iyisini yapacağım!")
-if cevap=="bilmiyorum":
+if cevap=="bilmiyorum ":
     print("Çok inatçı çıktın AQ!")
 
 # bu bir comment oluyormuş, bilgim olsun. Yarın, öbürgün işlem yapacağım zaman aklımda!
